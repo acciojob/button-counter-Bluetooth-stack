625996f5ea6fdc21fb258094b1ebb0d6f6542c7c
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-14 17:42:58 +0530

    render button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,10 +3,14 @@ import React, {useState} from "react";
 
 const Button = ()=>{
     let [clicked, setClicked] = useState(0);
+
+    function renderCount(){
+        setClicked(clicked+1);
+    }
     return (
         <div>
             <p>Button clicked {clicked} times</p>
-            <button onClick={setClicked(clicked+1)}>Click me</button>
+            <button onClick={renderCount}>Click me</button>
         </div>
     )
 }
